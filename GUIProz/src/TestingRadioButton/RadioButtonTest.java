1a2ffc6b459cfa85b7bba1bd128275fe7c9f32db
Scilent Knight <scilentknight512@gmail.com>
2025-11-04 17:22:53 +0545

    TestingRadioButton

---
+++ GUIProz/src/TestingRadioButton/RadioButtonTest.java
@@ -0,0 +1,11 @@
+package TestingRadioButton;
+import javax.swing.JFrame;
+
+public class RadioButtonTest {
+    public static void main(String[] args) {
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        radioButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        radioButtonFrame.setSize(300, 180);
+        radioButtonFrame.setVisible(true);
+    }
+}
