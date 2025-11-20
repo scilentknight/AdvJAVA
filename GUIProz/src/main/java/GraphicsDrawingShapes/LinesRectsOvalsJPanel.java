edd9a321a335e84d780f44a8903a3ad71e14e389
Scilent Knight <scilentknight512@gmail.com>
2025-11-20 09:24:41 +0545

    GraphicsDrawingShapes project added

---
+++ GUIProz/src/main/java/GraphicsDrawingShapes/LinesRectsOvalsJPanel.java
@@ -0,0 +1,26 @@
+package GraphicsDrawingShapes;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class LinesRectsOvalsJPanel extends JPanel{
+    public void paintComponent(Graphics g){
+        this.setBackground(Color.WHITE);
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawLine(5,30,380,30);
+        g.setColor(Color.BLUE);
+        g.drawRect(5,40,90,55);
+        g.fillRect(100,40,90,55);
+        g.setColor(Color.CYAN);
+        g.fillRoundRect(195,40,90,55,20,20);
+        g.drawRoundRect(290,40,90,55,20,20);
+        g.setColor(Color.GREEN);
+        g.draw3DRect(5,100,90,55,true);
+        g.fill3DRect(100,100,90,55,false);
+        g.setColor(Color.MAGENTA);
+        g.drawOval(195,100,90,55);
+        g.fillOval(290,100,90,55);
+    }
+}
