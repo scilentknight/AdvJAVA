aa871702f0eeb87450e50bb1714c784e1adbecc1
Scilent Knight <scilentknight512@gmail.com>
2025-11-20 09:39:29 +0545

    TestingCheckBox Package added

---
+++ GUIProz/src/main/java/TestingCheckBox/CheckBoxTest.java
@@ -0,0 +1,10 @@
+package TestingCheckBox;
+import javax.swing.JFrame;
+public class CheckBoxTest {
+    public static void main(String[] args) {
+        CheckBoxFrame checkBoxFrame = new CheckBoxFrame();
+        checkBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        checkBoxFrame.setSize(400, 150);
+        checkBoxFrame.setVisible(true);
+    }
+}
