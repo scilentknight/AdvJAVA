9ada171d5d1bddcf2e4d7833b3f225846af62ac7
Scilent Knight <scilentknight512@gmail.com>
2025-12-04 12:41:55 +0545

    TestingKeyEventHandling package added

---
+++ GUIProz/src/main/java/TestingKeyEventHandling/KeyEventDemoTest.java
@@ -0,0 +1,12 @@
+package TestingKeyEventHandling;
+
+import javax.swing.JFrame;
+
+public class KeyEventDemoTest {
+    public static void main(String[] args) {
+        KeyEventDemoFrame frame = new KeyEventDemoFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
